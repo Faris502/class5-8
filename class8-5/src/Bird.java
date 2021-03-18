9ae622797cf7e20be01b1fa5f2d4c9a2f9964a8f
faris <faris@DESKTOP-B8U0409>
2021-03-18 11:22:55 +0300

    2

--- class8-5/src/Bird.java
+++ class8-5/src/Bird.java
@@ -3,7 +3,7 @@ public class Bird {
 
 	String color;
     public void fly(){
-        System.out.println("Bird is flying in the sky freely");
+        System.out.println("Bird is flying");
     }
     public void tweet(){
         System.out.println("tweet");
